3dd7ce3d73d9bc36dc4a60cf9d0f9c38a0a297cb
zhangsanshi <zhangsanshi5566@gmail.com>
2015-04-12 01:56:51 +0800

    twoSum
    update twoSum

--- Two Sum/twoSum.js
+++ Two Sum/twoSum.js
@@ -5,20 +5,14 @@
  */
 var twoSum = function(numbers, target) {
     var result = {},
-        first, second;
+        first;
     for (var i = 0, len = numbers.length; i < len; i++) {
         first = numbers[i];
-        second = target - first;
-        if (!(first in  result)) {
-            result[first] = [i];
-            result['!' + (target - first)] = [i];
-        } else {
-            result[first].push(i);
-            result['!' + (target - first)].push(i);
-        }
-        if ('!' + first in result && i !== result['!' + first][0]) {
-            return [ result['!' + first][0] + 1, i + 1 ];
-        }
 
+        if (first in result) {
+            return [result[first] + 1, i + 1];
+        } else {
+            result[target - first] = i;
+        }
     }
 };
